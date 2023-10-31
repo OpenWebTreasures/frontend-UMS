0bb531b4a2a769929824541d33d2269dcadea235
hajalimazen__ <hadjalimazen@gmail.com>
2023-10-31 12:43:35 +0100

    adding dashboard layout structure

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,26 @@
+import Header from "./header";
+import SideBar from "./sidebar";
+
+
+function DashboardLayout() {
+    return (
+        <div className="flex h-screen overflow-hidden">
+
+            <SideBar />
+
+            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+
+                <Header />
+
+                <main>
+                    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+
+                        {/* childs */}
+                    </div>
+                </main>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardLayout;
